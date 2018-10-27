dc1445964c197b72f585a96483fcec9edeb43b89
Sreekant <geekykant@gmail.com>
2018-10-28 01:57:24 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,170 @@
+<html>
+    <head>
+        <title>Firebase | PHP Cloud Messaging</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="//www.gstatic.com/mobilesdk/160503_mobilesdk/logo/favicon.ico">
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+        <style type="text/css">
+            body{
+            }
+            div.container{
+                width: 1000px;
+                margin: 0 auto;
+                position: relative;
+            }
+            legend{
+                font-size: 30px;
+                color: #555;
+            }
+            .btn_send{
+                background: #00bcd4;
+            }
+            label{
+                margin:10px 0px !important;
+            }
+            textarea{
+                resize: none !important;
+            }
+            .fl_window{
+                width: 400px;
+                position: absolute;
+                right: 0;
+                top:100px;
+            }
+            pre, code {
+                padding:10px 0px;
+                box-sizing:border-box;
+                -moz-box-sizing:border-box;
+                webkit-box-sizing:border-box;
+                display:block;
+                white-space: pre-wrap;
+                white-space: -moz-pre-wrap;
+                white-space: -pre-wrap;
+                white-space: -o-pre-wrap;
+                word-wrap: break-word;
+                width:100%; overflow-x:auto;
+            }
+
+        </style>
+    </head>
+    <body>
+        <?php
+        // Enabling error reporting
+        error_reporting(-1);
+        ini_set('display_errors', 'On');
+
+        require_once __DIR__ . '/firebase.php';
+        require_once __DIR__ . '/push.php';
+
+        $firebase = new Firebase();
+        $push = new Push();
+
+        // optional payload
+        // $payload = array();
+        // $payload['country'] = 'America';
+        // $payload['score'] = '2.6';
+
+        // notification API_KEY
+        $API_KEY = isset($_GET['API_KEY']) ? $_GET['API_KEY'] : '';
+
+        // notification title
+        $topic = isset($_GET['topic']) ? $_GET['topic'] : '';
+
+        // notification intent_url
+        $intent_url = isset($_GET['intent_url']) ? $_GET['intent_url'] : '';
+
+        // notification title
+        $title = isset($_GET['title']) ? $_GET['title'] : '';
+
+        // notification image_url
+        $image_url = isset($_GET['image_url']) ? $_GET['image_url'] : '';
+
+        // notification message
+        $message = isset($_GET['message']) ? $_GET['message'] : '';
+
+        // push type - single user / topic
+        $push_type = isset($_GET['push_type']) ? $_GET['push_type'] : '';
+
+        // whether to include to image or not
+        $include_image = isset($_GET['include_image']) ? TRUE : FALSE;
+
+
+        $push->setTitle($title);
+        $push->setIntentURL($intent_url);
+        $push->setMessage($message);
+        if ($include_image) {
+            $push->setImage($image_url);
+        } else {
+            $push->setImage('');
+        }
+        $push->setIsBackground(FALSE);
+        // $push->setPayload($payload);
+
+
+        $json = '';
+        $response = '';
+
+        if ($push_type == 'topic') {
+            $json = $push->getPush();
+            $response = $firebase->sendToTopic($topic,$json,$API_KEY);
+        } else if ($push_type == 'individual') {
+            $json = $push->getPush();
+            $regId = isset($_GET['regId']) ? $_GET['regId'] : '';
+            $response = $firebase->send($regId, $json);
+        }
+        ?>
+        <div class="container">
+            <div class="fl_window">
+                <div><img src="img/firebase_logo.png" width="200" alt="Firebase"/></div>
+                <br/>
+                <?php if ($json != '') { ?>
+                    <label><b>Request:</b></label>
+                    <div class="json_preview">
+                        <pre><?php echo json_encode($json) ?></pre>
+                    </div>
+                <?php } ?>
+                <br/>
+                <?php if ($response != '') { ?>
+                    <label><b>Response:</b></label>
+                    <div class="json_preview">
+                        <pre><?php echo json_encode($response) ?></pre>
+                    </div>
+                <?php } ?>
+
+            </div>
+
+            <form class="pure-form pure-form-stacked" method="get">
+                <fieldset>
+
+                  <label for="API_KEY1">API KEY</label>
+                  <input type="text" id="API_KEY1" name="API_KEY" class="pure-input-1-2" placeholder="Enter API KEY">
+
+                    <legend>Send to Topic</legend>
+
+                    <label for="title1">Topic</label>
+                    <input type="text" id="topic1" name="topic" class="pure-input-1-2" placeholder="Enter topic">
+
+                    <label for="intent_url1">Intent URL</label>
+                    <input type="text" id="intent_url1" name="intent_url" class="pure-input-1-2" placeholder="Enter Intent URL">
+
+                    <label for="title1">Title</label>
+                    <input type="text" id="title1" name="title" class="pure-input-1-2" placeholder="Enter title">
+
+                    <label for="image_url">Image URL</label>
+                    <input type="text" id="image_url1" name="image_url" class="pure-input-1-2" placeholder="Enter Image URL">
+
+                    <label for="message1">Message</label>
+                    <textarea class="pure-input-1-2" name="message" id="message1" rows="5" placeholder="Notification message!"></textarea>
+
+                    <label for="include_image1" class="pure-checkbox">
+                        <input id="include_image1" name="include_image" type="checkbox"> Include image
+                    </label>
+                    <input type="hidden" name="push_type" value="topic"/>
+                    <button type="submit" class="pure-button pure-button-primary btn_send">Send to Topic</button>
+                </fieldset>
+            </form>
+        </div>
+    </body>
+</html>
